b335da6a977367166bda6721e42d6d75d54aeac2
SRonan <said.hamouch01@gmail.com>
2017-08-19 17:48:06 +0100

    said Modif jQuery Bootstrap

--- README.md
+++ README.md
@@ -1,33 +1,3 @@
 # projectLear
-Learn Project
-
-Ajouter un formulaire :
-    Nom
-    Prenom
-    Age : Number
-    Sexe : Male Ou Femme
-
-    Boutton : Soumettre
-
-    Une fiche qui apparait
-    Nom
-    Prenom
-    Le sexe
-    et Anneé de Naissance
-
-Ajouter un 2 eme formulaire :
-    Input Text :
-
-    Boutton : Soumettre
-
-    Return true ou false si palindrome text
-
-
-Ajouter un 3 eme formulaire :
-    Input Number : avec un bouton a coter  +
-    Si j'appuie sur + Input Text Max De 20
-    Possibilité de supprimer  
-
-    Boutton : Calculer la Somme
-
-    Return Sigma
+formulaire 1 et 3
+Jquery
--- main.js
+++ main.js
@@ -1,208 +1,148 @@
-/*PREMIER FORMULAIRE*/
-/*
-document.getElementById("sexe").value = "";
-//Ici il faut créer une fonction appart
-document.getElementById("premierBoutton").addEventListener("click", function (e) {
-  var prenomElement = document.createElement("p");
-  prenomElement.textContent = document.getElementById("prenom").value;
-  var nomElement = document.createElement("p");
-  nomElement.textContent = document.getElementById("nom").value;
-  var ageElement = document.createElement("p");
-  ageElement.textContent = document.getElementById("age").value;
-  var sexeElement = document.createElement("p");
-  sexeElement.textContent = document.getElementById("sexe").value;
-  if (prenomElement.textContent === ""){
-    document.getElementById("prenomHelp").textContent = "Veuillez entrer votre prénom!";
-  } else if (nomElement.textContent === ""){
-    document.getElementById("prenomHelp").textContent = "";
-    document.getElementById("nomHelp").textContent = "Veuillez entrer votre nom!";
-  } else if (ageElement.textContent === ""){
-    document.getElementById("nomHelp").textContent = "";
-    document.getElementById("ageHelp").textContent = "Veuillez entrer votre âge!";
-  } else if (sexeElement.textContent === ""){
-    document.getElementById("ageHelp").textContent = "";
-    document.getElementById("sexeHelp").textContent = "Veuillez choisir votre sexe!(Zamel makaynch :D)";
-  } else {
-    document.getElementById("prenomHelp").textContent = "";
-    document.getElementById("nomHelp").textContent = "";
-    document.getElementById("ageHelp").textContent = "";
-    document.getElementById("sexeHelp").textContent = "";
-document.getElementById("fichePremierFormulaire").innerHTML = '<h2> Votre fiche: </h2><p>Prénom: <span id="fichePrenom"></span></p><p id="ficheNom">Nom: </p><p id="ficheAge">Année de naissance: </p><p id="ficheSexe">Sexe: </p>';
-document.getElementById("fichePrenom").textContent += prenomElement.textContent;
-document.getElementById("ficheNom").textContent += nomElement.textContent;
-document.getElementById("ficheAge").textContent += 2017 - ageElement.textContent;
-document.getElementById("ficheSexe").textContent += sexeElement.textContent;
-document.getElementById("fichePremierFormulaire").style = "display: block; border: 2px solid black; padding-left: 10px; margin-top: 5px;";
-}
-});
-
-document.getElementById("premierBouttonEffacer").addEventListener("click", function (e) {
-document.getElementById("fichePremierFormulaire").innerHTML = '<h2> Votre fiche: </h2><p>Prénom: <span id="fichePrenom"></span></p><p id="ficheNom">Nom: </p><p id="ficheAge">Année de naissance: </p><p id="ficheSexe">Sexe: </p>'
-document.getElementById("prenom").value = "";
-document.getElementById("nom").value = "";
-document.getElementById("age").value = "";
-document.getElementById("sexe").value = "";
-});
-
-*/
-//**************************************************************/
 /*PREMIER FORMULAIRE By SAID*/
-document.getElementById("soumettreBoutton").addEventListener("click", soumettreFunction);
-document.getElementById("effacerBoutton").addEventListener("click", effacerFunction);
+
+$('#soumettreBoutton').click(function() {
+  soumettreFunction();
+});
+$('#effacerBoutton').click(function() {
+  effacerFunction();
+});
 
 var donneesArray = ["prenom", "nom", "age", "sexe"];
 var donneesArrayElement = [];
-for (var i =0; i < donneesArray.length; i++ ) {
+for (var i = 0; i < donneesArray.length; i++) {
   donneesArrayElement[i] = document.createElement("p");
-  }
+}
 
-function soumettreFunction () {
-	var valididy = isValid();
-    if(valididy){
-		for (var i =0; i < donneesArray.length; i++ ) {
-		if (document.getElementById(donneesArray[i]) === age) {
-			var birthYear = 2017 - document.getElementById(donneesArray[i]).value;
-     donneesArrayElement[i].textContent = donneesArray[i].toUpperCase() +": " + birthYear;
-     document.getElementById("fichePremierFormulaire").appendChild(donneesArrayElement[i]);
-		} else {
-     donneesArrayElement[i].textContent = donneesArray[i].toUpperCase() +": " + document.getElementById(donneesArray[i]).value;
-     document.getElementById("fichePremierFormulaire").appendChild(donneesArrayElement[i]);
-        }
+function soumettreFunction() {
+  $('#fichePremierFormulaire').attr("class", "jumbotron");
+  $("#fichePremierFormulaire").css({
+    'display': 'block',
+    'border': '2px solid black',
+    'margin-top': '5px'
+  });
+
+  var valididy = isValid();
+  if (valididy) {
+    for (var i = 0; i < donneesArray.length; i++) {
+      if (document.getElementById(donneesArray[i]) === age) {
+        var birthYear = 2017 - $('.input:eq(' + i + ')').val();
+        donneesArrayElement[i].textContent = "ANNEE DE NAISSANCE: " + birthYear;
+      } else {
+        donneesArrayElement[i].textContent = donneesArray[i].toUpperCase() + ": " + $('.input:eq(' + i + ')').val();
+      }
+      $('#fichePremierFormulaire').append(donneesArrayElement[i]);
     }
-}
-}
-
-function effacerFunction () {
-		for (var i =0; i < donneesArray.length; i++ ) {
-	document.getElementById(donneesArray[i]).value = "";
-	donneesArrayElement[i].textContent = "";
-    document.getElementById("fichePremierFormulaire").appendChild(donneesArrayElement[i]);
-    document.getElementById(donneesArray[i]+"Help").textContent = "";
-	}
+  }
 }
 
-function isValid () {
-	var testValidity = true;
-	for (var i =0; i < donneesArray.length; i++ ) {
-		if (document.getElementById(donneesArray[i]).value === "") {
-			document.getElementById(donneesArray[i]+"Help").textContent = "Veuillez entrer votre " + donneesArray[i].toUpperCase() + "!";
-		} else {
-		document.getElementById(donneesArray[i]+"Help").textContent = "";
-		}
-	testValidity = testValidity && (document.getElementById(donneesArray[i]).value !== "");
-	}
-	return testValidity;
+function effacerFunction() {
+  $('#fichePremierFormulaire').html('');
+  for (var i = 0; i < donneesArray.length - 1; i++) {
+    $('.input:eq(' + i + ')').val('');
+    $('#' + donneesArray[i] + 'Help').text("");
+  }
+}
+
+function isValid() {
+  var testValidity = true;
+  for (var i = 0; i < donneesArray.length; i++) {
+    if (($('.input:eq(' + i + ')').val() == '') || ($('.input:eq(' + i + ')').text() == null)) {
+      $('#' + donneesArray[i] + 'Help').text("Veuillez entrer votre " + donneesArray[i].toUpperCase() + "!");
+    } else {
+      $('#' + donneesArray[i] + 'Help').text("");
+    }
+    testValidity = testValidity && ($('.input:eq(' + i + ')').val() != "") && ($('.input:eq(' + i + ')').text() !== null);
+  }
+  return testValidity;
 }
 
 
 
 /*******************************************************************/
-/*PREMIER FORMULAIRE By Anas*/
-/*
-document.getElementById("premierBouttonAnas").addEventListener("click",doOnClick)
-
-//Jamais ecrire une fonction Call back directement dans l'evenement
-//Ici doOnClick est un callback donc je le met apart
-
-//Il faut aussi toujours à penser à ecrire du code groupe si tu fait du copier coller
-//C'est que tu peut ecrir une fonction ici createElement est unbon example
-function doOnClick(e){
-    // Cette technique de ecrire un tableau avec tous les ellement permet de boucler sur un tableau
-    // et facilement trouver les bugs mais surtout facilement ajouter un champs dans le fomulaire
-    // Toujours penser à la maintenabilité du Code !!!
-    var formFields =["prenom","nom","age","sexe"]
-    for (var i = 0; i < formFields.length; i++) {
-      createElement(formFields[i]);
-    }
-    var formValid = isFormValid(formFields);
-    if(formValid){
-      // Cette partie n'est pas terrible puisque si j'ajoute un ellement dans formFields ca va pas etre pris en compte
-      // A toi de corriger cette partie
-      document.getElementById("fichePremierFormulaire").innerHTML = '<h2> Votre fiche: </h2><p>Prénom: <span id="fichePrenom"></span></p><p id="ficheNom">Nom: </p><p id="ficheAge">Année de naissance: </p><p id="ficheSexe">Sexe: </p>';
-      document.getElementById("fichePrenom").textContent += document.getElementById("prenom").value ;
-      document.getElementById("ficheNom").textContent += document.getElementById("nom").value ;
-      document.getElementById("ficheAge").textContent += 2017 - document.getElementById("age").value ;
-      document.getElementById("ficheSexe").textContent += document.getElementById("sexe").value ;
-      document.getElementById("fichePremierFormulaire").style = "display: block; border: 2px solid black; padding-left: 10px; margin-top: 5px;";
-    }
-}
-//Create Ellement est une fonction qui créé un ellement a base d'un nom
-function createElement(name){
-  var theElement = document.createElement("p");
-  theElement.textContent = document.getElementById(name).value;
-  return theElement;
-}
-//Cette fonction est plus simple est permet de tester si la form est valid
-function isFormValid(formFields){
-  var isValid = true;
-  for (var i = 0; i < formFields.length; i++) {
-    if(document.getElementById(formFields[i]).value  === ""){
-      document.getElementById(formFields[i] + "Help").textContent = "Veuillez entrer votre " + formFields[i]+ "!";
-    }else{
-      document.getElementById(formFields[i] + "Help").textContent = "";
-    }
-    isValid = isValid && (document.getElementById(formFields[i]).value  != "")
-  }
-  return isValid;
-}
-*/
 /*******************************************************************/
 
 /* 2EME FORMULAIRE*/
-/*
-document.getElementById("palindromeBoutton").addEventListener("click", function (e) {
+
+$("#palindromeBoutton").click(function() {
+  console.log("testetste");
+  $('#ficheDeuxiemeFormulaire').attr("class", "jumbotron");
   var palindromeElement = document.createElement("p");
-  palindromeElement.textContent = document.getElementById("palindrome").value;
-if (palindromeElement.textContent === ""){
-  document.getElementById("palindromeHelp").textContent = "Veuillez entrer votre texte!";
-} else {
-  document.getElementById("palindromeHelp").textContent = "";
-  var resultatElement = document.createElement("span");
-  var testTextElement = document.createElement("string");
-  testTextElement = document.getElementById("palindrome").value;
-  var number = 0;
-    for (var counter1 = 0; counter1 < Math.trunc(testTextElement.length/2); counter1++)
-    {
+  palindromeElement.textContent = $("#palindrome").val();
+  if (palindromeElement.textContent === "") {
+    $("#palindromeHelp").text("Veuillez entrer votre texte!");
+  } else {
+    $("#palindromeHelp").text("");
+    var resultatElement = document.createElement("span");
+    var testTextElement = document.createElement("string");
+    testTextElement = $("#palindrome").val();
+    var number = 0;
+    for (var counter1 = 0; counter1 < Math.trunc(testTextElement.length / 2); counter1++) {
       if (testTextElement[counter1] === testTextElement[testTextElement.length - 1 - counter1]) {
-          console.log(counter1*1000+1000);
-      } else {number = 1}
+        console.log(counter1 * 1000 + 1000);
+      } else {
+        number = 1
+      }
     }
 
-if (number === 0){
-  resultatElement.textContent = "est un palindrome.";
-} else {
-  resultatElement.textContent = "n'est pas un palindrome.";
-}
-document.getElementById("ficheDeuxiemeFormulaire").innerHTML = '<h2> Résultat: </h2><p id="fichePalindrome">Votre texte </p>';
-document.getElementById("fichePalindrome").appendChild(resultatElement);
-document.getElementById("ficheDeuxiemeFormulaire").style = "display: block; border: 2px solid black; padding-left: 10px; margin-top: 5px;";
-}
+    if (number === 0) {
+      resultatElement.textContent = "est un palindrome.";
+    } else {
+      resultatElement.textContent = "n'est pas un palindrome.";
+    }
+    $("#ficheDeuxiemeFormulaire").html('<h2> Résultat: </h2><p id="fichePalindrome">Votre texte </p>');
+    $("#fichePalindrome").append(resultatElement);
+    $("#ficheDeuxiemeFormulaire").css({
+      'display': 'block',
+      'border': '2px solid black',
+      'padding-left': '10px',
+      'margin-top': '5px'
+    });
+  }
 });
 
-document.getElementById("palindromeBouttonEffacer").addEventListener("click", function (e) {
-document.getElementById("ficheDeuxiemeFormulaire").innerHTML = '<h2>Résultat: </h2><br />';
-document.getElementById("palindrome").value = "";
+$("#palindromeBouttonEffacer").click(function() {
+  $("#ficheDeuxiemeFormulaire").html('<h2>Résultat: </h2><br />');
+  $("#palindrome").val("");
 });
-*/
+
 
 /* 3EME FORMULAIRE*/
-/*
-var buttonObject = {
-  textHTML: '<label for="somme1">Nombre: </label><input type="number" name="somme1" id="somme1" placeholder="Nombre" required> <button id="plus1" style="display: default;">+</button> <span id="somme1Help"></span>',
-
-};
-
 
 var numberTestClick = 1;
-for (var i = 1; i < 2; i++ ) {
-document.getElementById("plus"+numberTestClick).addEventListener("click", function (e) {
-  var objectElement1 = document.createElement("string");
-  var number2 = Object.create(buttonObject);
-  document.querySelector
-  objectElement1.textContent = number2.textHTML;
-document.getElementById("autresNombres").innerHTML = objectElement1.textContent;
-console.log("plus"+numberTestClick+"test");
-
+$('#plus1').click(function(e) {
+  e.preventDefault();
+  addNumber();
+})
+$('#moins1').click(function(e) {
+  e.preventDefault();
+  deleteNumber();
+})
+$('#calculerSomme').click(function(e) {
+  e.preventDefault();
+  calculSomme();
 });
+
+function calculSomme() {
+  $('#ficheSomme').attr("class", "jumbotron");
+  var sigma = 0;
+  for (i = 1; i < numberTestClick + 1; i++) {
+    sigma = sigma + Number($('#somme' + i).val());
+  }
+  $('#ficheSomme').html('<h2> Résultat: </h2><p id="sommeCalculee">La somme = ' + sigma + '</p>');
+  $('#ficheSomme').css('display', 'block');
+
 }
-*/
+
+function addNumber() {
+  numberTestClick = numberTestClick + 1;
+  $('#autresNombres').append('<form id=form' + numberTestClick + ' ></form>');
+  $('#form' + numberTestClick).append('<label for="somme' + numberTestClick + '">Nombre: ' + numberTestClick + ' </label>');
+  $('#form' + numberTestClick).append('<input type="number" name="somme' + numberTestClick + '" class="input form-control" id="somme' + numberTestClick + '" placeholder="Nombre" required>');
+}
+
+function deleteNumber() {
+  if (numberTestClick > 1) {
+    $('#form' + numberTestClick).remove();
+    numberTestClick = numberTestClick - 1;
+  }
+}
